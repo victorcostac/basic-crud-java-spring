22bd02efbd7e33062b9b259d22911f69842211ca
Victor Costa <victorcostadecoura@gmail.com>
2024-02-20 09:47:21 -0300

    GET/POST/PUT/DELETE METHODS

--- crud/src/main/java/com/example/crud/controllers/ProductController.java
+++ crud/src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,13 +1,24 @@
 package com.example.crud.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 
 
@@ -20,11 +31,44 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity getAllProducts(){
-        var AllProducts = repository.findAll();
-        return ResponseEntity.ok(AllProducts);
+        var AllProducts = repository.findAllByActiveTrue();
         
+        return ResponseEntity.ok(AllProducts);   
     }
-
-    // aqui !!!
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok(newProduct);
+    }
+    //PAREI AQUI!!!!!!!!
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){
+        System.out.println(data);
+        
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        Optional<Product> optionalProduct = repository.findById(id);
+        if(optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            product.setActive(false);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
