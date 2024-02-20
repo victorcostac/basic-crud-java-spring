7325c6387ccc31ed1f89380ff71dddac9e528404
Victor Costa <victorcostadecoura@gmail.com>
2024-02-20 09:46:09 -0300

    RequestProduct record created

---
+++ crud/src/main/java/com/example/crud/domain/product/RequestProduct.java
@@ -0,0 +1,13 @@
+package com.example.crud.domain.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestProduct( 
+        String id,
+        @NotBlank
+        String name, 
+        @NotNull
+        float price_in_cents) {
+    
+}
